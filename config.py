a2eab8596f60de2f65bc134a79188e7c575f5403
Caleo Silva <caleosilva75@gmail.com>
2023-09-12 14:26:52 -0300

    fix: alterando o ip

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-socket_host = '127.0.0.1'
+socket_host = '172.16.103.6'
 socket_port = 2222
 
-socket_rfid_client_host = '127.0.0.1'
+socket_rfid_client_host = '172.16.103.0'
 socket_rfid_client_port = 1111
